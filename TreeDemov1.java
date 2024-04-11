8a1cb2455886eabec375bb62ef80c7ed3531bc54
Telmen17 <telmen7948@gmail.com>
2024-04-11 15:12:52 -0700

    Implemented getMin() function.

--- TreeDemov1.java
+++ TreeDemov1.java
@@ -18,26 +18,25 @@ class Node{
  *Class for the BST.
  */
 class BinarySearchTree{
+	Node root;
 
-	   Node root;
-
-	   /**
-	 	*Inserts the node into the tree.
-	 	* @param value The integer value to be inserted into the BST.
-	 	*/
-	   public void insert(int value){
-	      //tree is empty
-	      if(root == null){
-	         root = new Node(value);
-	         return;
-	      }else{
-	         Node current = root;
-	         Node parent = null;
+	/**
+	 *Inserts the node into the tree.
+	 * @param value The integer value to be inserted into the BST.
+	 */
+	public void insert(int value){
+		//tree is empty
+		if(root == null){
+			root = new Node(value);
+			return;
+		}else{
+			Node current = root;
+			Node parent = null;
 	         
-	         while(true){
-	            parent = current;
-	            
-	            if(value < current.value){
+			while(true){
+				parent = current;
+
+				if(value < current.value){
 	               current = current.left;
 	               if(current == null){
 	                  parent.left = new Node(value);
@@ -51,95 +50,101 @@ class BinarySearchTree{
 	               }
 	            }
 	           
-	         }//closing while
+			}//closing while
 	      
-	      }//closing main if-else 
-	   }
+		}//closing main if-else
+	}
 
-	   /**
-	 	*A method which traverses the BST and prints out the nodes in preorder.
-	 	* @param root The root node of the BST.
-	 	*/
-	   public void preOrderTraversal(Node root){
-		   //Preorder: Current, Left, Right
-		   if (root != null) {
-			   //Printing current value
-			   System.out.println(root.value + " ");
-			   //Traversing left subtree
-			   preOrderTraversal(root.left);
-			   //Traversing right subtree
-			   preOrderTraversal(root.right);
-		   }
-	   }
-	   /**
-	 	* A method which traverses the BST and prints out the nodes as inorder.
-	 	* @param root The root node of the BST.
-	 	*/
-	   public void inOrderTraversal(Node root) {
-		   //Inorder: Left, Current, Right
-			if (root != null) {
-				//Traversing left subtree
-				inOrderTraversal(root.left);
-				//Printing current value
-				System.out.println(root.value + " ");
-				//Traversing right subtree
-				inOrderTraversal(root.right);
-			}
-	   }
-
-
-	    /**
-	 	 * A method which traverses the BST and prints out the nodes as postorder.
-	 	 * @param root The root node of the BST.
-	 	 */
-		public void postOrderTraversal(Node root) {
-			//Postorder: Left, Right, Current
-			if (root != null) {
-				//Traversing left subtree
-				postOrderTraversal(root.left);
-				//Traversing right subtree
-				postOrderTraversal(root.right);
-				//Printing current value
-				System.out.println(root.value + " ");
-			}
-		   
-	   }
-
-
-	    /**
-	 	 * A method to find the node in the tree with a specific value.
-		 * @param root The root node of the BST.
-	 	 * @param key The value to find in the BST
-	 	 * @return Returns true if the node is found, or false if it is not found.
-	 	 */
-		public boolean find(Node root, int key){
-			//If the current node is null, return false
-			if (root == null){
-				return false;
-			}
-			//If the current node is found, return true
-			if (key == root.value){
-				return true;
-			}
-			//If the key is less, traverse left
-			if (key < root.value) {
-				return find(root.left, key);
-			}else {
-				//If the key is larger, traverse right
-				return find(root.right, key);
-			}
+	/**
+	 *A method which traverses the BST and prints out the nodes in preorder.
+	 * @param root The root node of the BST.
+	 */
+	public void preOrderTraversal(Node root){
+		//Preorder: Current, Left, Right
+		if (root != null) {
+			//Printing current value
+			System.out.println(root.value + " ");
+			//Traversing left subtree
+			preOrderTraversal(root.left);
+			//Traversing right subtree
+			preOrderTraversal(root.right);
 		}
-	   
-	   
-	   
-	   /*
-	   a method to find the node in the tree
-	   with a smallest key
-	   */
-	   public int getMin(Node root){
-         //implement in here
-	      
-	   }
+	}
+	/**
+	 * A method which traverses the BST and prints out the nodes as inorder.
+	 * @param root The root node of the BST.
+	 */
+	public void inOrderTraversal(Node root) {
+		//Inorder: Left, Current, Right
+		if (root != null) {
+			//Traversing left subtree
+			inOrderTraversal(root.left);
+			//Printing current value
+			System.out.println(root.value + " ");
+			//Traversing right subtree
+			inOrderTraversal(root.right);
+		}
+	}
+
+	/**
+	 * A method which traverses the BST and prints out the nodes as postorder.
+	 * @param root The root node of the BST.
+	 */
+	public void postOrderTraversal(Node root) {
+		//Postorder: Left, Right, Current
+		if (root != null) {
+			//Traversing left subtree
+			postOrderTraversal(root.left);
+			//Traversing right subtree
+			postOrderTraversal(root.right);
+			//Printing current value
+			System.out.println(root.value + " ");
+		}
+	}
+
+	/**
+	 * A method to find the node in the tree with a specific value.
+	 * @param root The root node of the BST.
+	 * @param key The value to find in the BST
+	 * @return Returns true if the node is found, or false if it is not found.
+	 */
+	public boolean find(Node root, int key){
+		//If the current node is null, return false
+		if (root == null){
+			return false;
+		}
+		//If the current node is found, return true
+		if (key == root.value){
+			return true;
+		}
+		//If the key is less, traverse left
+		if (key < root.value) {
+			return find(root.left, key);
+		}else {
+			//If the key is larger, traverse right
+			return find(root.right, key);
+		}
+	}
+
+
+	/**
+	 * A method to find the node in the tree with the smallest key.
+	 * @param root The root node of the BST.
+	 * @return The smallest key in the BST.
+	 */
+	public int getMin(Node root) {
+		//Empty BST
+		if (root == null){
+			throw new IllegalArgumentException("The BST is empty.");
+		}
+		//Current node variable
+		Node cur = root;
+		//BST traversal
+		while (cur.left != null) {
+			cur = cur.left;
+		}
+		return cur.value;
+	}
 	  
 	  
 	  
